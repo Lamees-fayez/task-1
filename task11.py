0e998ae50a2c5c262e532deb1c03f0eb62d541f5
lamees-fayez <lameesfayez@gmail.com>
2024-08-05 20:20:09 +0300

    Added task11

--- task11.py
+++ task11.py
@@ -1,8 +1,17 @@
-numbers=(12, 75, 150, 180, 145, 525, 500)
-for num in numbers:
-    if numbers % 5 == 0:
-        print(num)
-    if numbers > 150:
-        print(num)
-    if numbers > 500 :
-        break       
+# numbers=[12, 75, 150, 180, 145, 525, 500ٍ]
+# for num in numbers:
+#     if numbers % 5 == 0:
+#         print(num)
+#     if numbers > 150:
+#         print(num)
+#     if numbers > 500 :
+#       break     
+#Q4
+numbers=[-10,-9,-8,-7,-6,-5,-4,-3,-2,-1]
+for i in numbers:
+    print(i)
+#Q5
+num=[1,2,3,4,5,6,7,8,9,10]
+for i in num:
+    i *= i*2
+    print(i)
